13f2cd7f572c4454966abf127c8b635df67a93d4
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-22 11:34:37 +0200

    chore: Setup linting with rules and config

--- src/ping.ts
+++ src/ping.ts
@@ -1,12 +1,12 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
 
-export const main: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello world!',
+export const main: APIGatewayProxyHandler = async (event) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: "Hello world!",
       input: event,
-    }, null, 2),
-  };
-}
+    },
+  ),
+});
