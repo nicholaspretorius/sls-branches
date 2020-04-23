d67a83d7c22de279e99e0337bd72a6c6721c23d9
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-23 14:38:22 +0200

    tests: Add unit test for handler

--- tests/unit/ping.unit.test.ts
+++ tests/unit/ping.unit.test.ts
@@ -1,8 +1,21 @@
-import { getGreeting } from "../../src/ping";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import context from "aws-lambda-mock-context";
+
+import { main, getGreeting } from "../../src/ping";
 
 describe("unit: /ping", () => {
-  it("should return a message of 'Hello world!'", () => {
+  it("getGreeting() should return a message of 'Hello world!'", () => {
     expect.hasAssertions();
     expect(getGreeting()).toBe("Hello world!");
+  });
+
+  it("endpoint should return a response with statusCode 200 and body.message", async () => {
+    const event: APIGatewayProxyEvent = null;
+    const ctx = context();
+    const response = { message: "Hello world!" };
+    const res = await main(event, ctx, null) as APIGatewayProxyResult;
+    expect(res).toBeDefined();
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toStrictEqual(JSON.stringify(response));
   });
 });
