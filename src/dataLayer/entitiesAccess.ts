5089bb784cd17f747a5150f3c4306baf1b126d01
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-28 18:00:56 +0200

    feat: Add DEL /entities/:entityId and fix create

--- src/dataLayer/entitiesAccess.ts
+++ src/dataLayer/entitiesAccess.ts
@@ -16,20 +16,21 @@ export default class EntityAccess {
   ) { }
 
   async createEntity(entity: Entity): Promise<Entity> {
+    // console.log("createEntity entity: ", entity);
     const params = {
       TableName: this.entitiesTable,
       Item: entity,
-      ReturnValues: "ALL_NEW",
+      ReturnValues: "ALL_OLD",
     };
 
     const res = await this.docClient.put(params).promise();
-
+    // console.log("POST /entities Res: ", res);
     return res.Attributes as Entity;
   }
 
   async getEntities(): Promise<ItemList> {
     const params = {
-      TableName: ENTITIES,
+      TableName: this.entitiesTable,
       KeyConditionExpression: "userId = :userId",
       ExpressionAttributeValues: {
         ":userId": "abc123", // event.requestContext.identity.cognitoIdentityId
@@ -43,7 +44,7 @@ export default class EntityAccess {
 
   async getEntityById(entityId: string): Promise<Entity> {
     const params = {
-      TableName: ENTITIES,
+      TableName: this.entitiesTable,
       Key: {
         entityId,
         userId: "abc123",
@@ -53,5 +54,19 @@ export default class EntityAccess {
     const res = await this.docClient.get(params).promise();
     // console.log("Res for entityId ", entityId, " : ", res);
     return res.Item as Entity;
+  }
+
+  async deleteEntityById(entityId: string) {
+    const params = {
+      TableName: this.entitiesTable,
+      Key: {
+        entityId,
+        userId: "abc123",
+      },
+    };
+
+    const res = await this.docClient.delete(params).promise();
+    console.log("DEL Res: ", res);
+    return res;
   }
 }
--- src/lambda/http/entities/create.ts
+++ src/lambda/http/entities/create.ts
@@ -2,22 +2,24 @@ import "source-map-support/register";
 
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-import { EntityCreateRequest } from "../../../models/entities/Entity";
+// import { EntityCreateRequest } from "../../../models/entities/Entity";
 import entityClient from "../../../businessLogic/entities";
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newEntity: EntityCreateRequest = JSON.parse(JSON.stringify(event.body));
+  const newEntity = JSON.parse(event.body);
   const userId = "abc123";
+  console.log(userId, newEntity, typeof newEntity);
 
   try {
     const entity = await entityClient.create(newEntity, userId);
-
+    console.log("POST /entities Res: ", entity);
+    // TODO: return created item in response.
     return {
       statusCode: 201,
       body: JSON.stringify(entity),
     };
   } catch (error) {
-    // console.error(error);
+    console.log("Error: ", error);
 
     return {
       statusCode: 400,
