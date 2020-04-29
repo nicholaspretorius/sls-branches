5bd1f7f818eca3350a0c1915960c8c85358692e7
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-29 11:08:11 +0200

    chore: Cleanup [skip ci]

--- src/lambda/http/entities/create.ts
+++ src/lambda/http/entities/create.ts
@@ -8,18 +8,18 @@ import entityClient from "../../../businessLogic/entities";
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newEntity = JSON.parse(event.body);
   const userId = "abc123";
-  console.log(userId, newEntity, typeof newEntity);
+  // console.log(userId, newEntity, typeof newEntity);
 
   try {
     const entity = await entityClient.create(newEntity, userId);
-    console.log("POST /entities Res: ", entity);
+    // console.log("POST /entities Res: ", entity);
     // TODO: return created item in response.
     return {
       statusCode: 201,
       body: JSON.stringify(entity),
     };
   } catch (error) {
-    console.log("Error: ", error);
+    // console.log("Error: ", error);
 
     return {
       statusCode: 400,
