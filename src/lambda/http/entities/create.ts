030e0bf0d721dd30a0b77a51fa8f577de80bf4f1
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-30 14:51:01 +0200

    feat: Add logger and cors to existing endpoints

--- src/lambda/http/entities/create.ts
+++ src/lambda/http/entities/create.ts
@@ -1,17 +1,21 @@
 import "source-map-support/register";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+const middy = require("middy");
+import { cors } from "middy/middlewares";
 
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
-// import { EntityCreateRequest } from "../../../models/entities/Entity";
+import { createLogger } from "../../../libs/logger";
 import entityClient from "../../../businessLogic/entities";
 
-export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const logger = createLogger("entities: getList");
+
+export const main = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newEntity = JSON.parse(event.body);
   const userId = "abc123";
   // console.log(userId, newEntity, typeof newEntity);
 
   try {
     const entity = await entityClient.create(newEntity, userId);
+    logger.info("Res: ", { entity });
     // console.log("POST /entities Res: ", entity);
     // TODO: return created item in response.
     return {
@@ -20,10 +24,17 @@ export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):
     };
   } catch (error) {
     // console.log("Error: ", error);
-
+    logger.error("Error::create: ", { error });
     return {
       statusCode: 400,
       body: JSON.stringify({ message: "There was an error creating the entity" }),
     };
   }
-};
+});
+
+main.use(
+  cors({
+    credentials: true,
+    origin: "*"
+  })
+);
--- src/lambda/http/entities/update.ts
+++ src/lambda/http/entities/update.ts
@@ -1,24 +1,39 @@
 import "source-map-support/register";
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+const middy = require("middy");
+import { cors } from "middy/middlewares";
 
+import { createLogger } from "../../../libs/logger";
 import entityClient from "../../../businessLogic/entities";
 
-export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const logger = createLogger("entities: update");
+
+export const main = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const { entityId } = event.pathParameters;
   const data = JSON.parse(event.body);
-  console.log("UPDATE params: ", entityId, data);
+
   try {
     const updateEntity = await entityClient.update(entityId, data);
-    console.log("Updated: ", updateEntity);
+    logger.info("Res: update: ", { entity: updateEntity });
+
     return {
       statusCode: 200,
       body: JSON.stringify(updateEntity),
     };
   } catch (error) {
     // console.log("Error: ", error);
+    logger.error("Error::update: ", { error });
+
     return {
       statusCode: 400,
       body: JSON.stringify({ message: "There was an error updating the entity" }),
     };
   }
-};
+});
+
+main.use(
+  cors({
+    credentials: true,
+    origin: "*"
+  })
+);
